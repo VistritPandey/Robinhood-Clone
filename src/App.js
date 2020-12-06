8959dec0af4a9809752ed1c31500ac29da0e6a3c
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-06 19:16:16 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
         <div className="app__header">
-          {/*Header*/}
+          <Header />
         </div>
         <div className="app__body">
 
