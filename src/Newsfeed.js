260b20fcbfa0097f80ec2f823f2c8214cc0783ca
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2020-12-11 14:30:58 +0530

    ok

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -6,7 +6,10 @@ function NewsFeed() {
         <div className="newsfeed">
             <div className="newsfeed__container">
                 <div className="newsfeed__chartSection">
-                    <h3></h3>
+                    <div className="newsfeed__portfolio">
+                        <h1>$123,321</h1>
+                        <p>+$25.52 (+0.05%) Today</p>
+                    </div>
                 </div>
             </div>
         </div>
