709b2e12b870135039d03f4bf7807fb76490b824
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2020-12-11 15:02:59 +0530

    OK

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -3,6 +3,16 @@ import {Line} from 'react-chartjs-2'
 
 
 function LineGraph() {
+
+    const data = 
+    [{
+        x: 10,
+        y: 20
+    },{
+        x: 15,
+        y: 10
+    }]
+
     return (
         <div className="linegraph">
             <Line 
@@ -10,9 +20,30 @@ function LineGraph() {
                     datasets: [
                         {
                             type: "line",
-                            data: data
+                            data: data,
+                            backgroundColor: "black",
+                            borderColor: "#5AC53B",
+                            borderWidth: 2,
+                            pointBorderColor: "rgba(0, 0, 0, 0)",
+                            pointBackgroundColor: "rgba(0, 0, 0, 0)",
+                            pointHoverBackgroundColor: "#5AC53B",
+                            pointHoverBorderColor: "#000000",
+                            pointHoverBorderWidth: 4,
+                            pointHoverRadius: 6
                         }
                     ]
+                }}
+                options = {{
+                    legend : {
+                        display: false
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                display: false
+                            }
+                        }]
+                    }
                 }}
             />
         </div>
