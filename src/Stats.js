df6e7cd5ebe09f21d324c04679f0ee3fa246d6ee
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2020-12-12 15:32:34 +0530

    k

--- src/Stats.js
+++ src/Stats.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './Stats.css'
 import axios from 'axios'
 import StatsRow from './StatsRow'
+import db from './firebase';
 
 const TOKEN = "bva7pa748v6us1m9hse0";
 const BASE_URL = "https://finnhub.io/api/v1/quote"
@@ -9,11 +10,21 @@ const BASE_URL = "https://finnhub.io/api/v1/quote"
 function Stats() {
 
     const [stockData, setstockData] = useState([]);
+    const [myStocks, setMyStocks] = useState([]);
+
     const getStockData = (stock) => {
         return axios
         .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
         .catch((error) => {
             console.error("Error", error.message)
+        })
+    }
+
+    const getMyStocks = () => {
+        db
+        .collecton('myStocks')
+        .onSnapshot(snapshot => {
+            console.log(snapshot)
         })
     }
 
