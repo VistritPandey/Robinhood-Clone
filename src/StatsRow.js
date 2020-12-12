e78d7752860ea98a00e44f38ba0577e4a260001f
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2020-12-12 15:10:23 +0530

    k

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './StatsRow.css'
 
-function StatsRow() {
+function StatsRow(props) {
     return (
-        <div>
-            
+        <div className="row">
+            <div className="row__intro">
+                <h1>{props?.name}</h1>
+                <p>{props.volume && (props.volume + "shares")}</p>
+            </div>
+            <div className="row__chart">
+                {/* <img src={StockChart} height={16} /> */}
+            </div>
+            <div className="row__numbers">
+                <p className="row__price">{props.price}</p>
+                <p className="row__percentage"> +{Number(percentage).toFixed(2)}%</p>
+            </div>
         </div>
     )
 }
